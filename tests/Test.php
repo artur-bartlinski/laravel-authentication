d7ab91533b97698d96a284dc223629a0ba122920
William Gu <william.gu@itrm.co.uk>
2017-10-16 12:19:56 +0100

    Code task

---
+++ tests/Test.php
@@ -0,0 +1,18 @@
+<?php
+
+    namespace ItrmCodeTaskTests;
+
+    /**
+     * Class test
+     * @package ItrmCodeTaskTests
+     */
+    class Test extends \PHPUnit_Framework_TestCase
+    {
+        /**
+         * @test
+         */
+        public function userCanRegister()
+        {
+            $this->assertTrue(true);
+        }
+    }
